16912496496192c9ef939c0cca56b3d1c3e5f140
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-05 16:49:56 +0900

    first commit

---
+++ src/test/java/com/ncookie/asyncpractice/AsyncPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncookie.asyncpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
